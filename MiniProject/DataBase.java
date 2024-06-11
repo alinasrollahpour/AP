1bf478cda428d77ec93dddcb5a9210dd6be22486
Ali-Zol <ali.zolfa.313@gmail.com>
2024-06-11 17:58:46 +0330

    Improvement Database class and adding addStudent and studentLoader methods to it

--- MiniProject/DataBase.java
+++ MiniProject/DataBase.java
@@ -1,19 +1,50 @@
 package MiniProject;
 
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+
 public class DataBase
 {
-//	private DataBase db = new DataBase();
-//
-//	private DataBase()
-//	{}
-//
-//	public DataBase getDb()
-//	{
-//		return db;
-//	}
-
-	public static void addStudent(Student student)
+	public static void addStudent(Student student) throws IOException, ClassNotFoundException
 	{
-		
+		Set<Student> students = new HashSet<>();
+		try
+		{
+			students = studentLoader();
+		}
+		catch(EOFException _)
+		{}
+		finally
+		{
+			for(Student s: students)
+			{
+				if(s.equals(student))
+					return;
+			}
+
+			students.add(student);
+		}
+
+		FileOutputStream fos = new FileOutputStream("Files/students.txt");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+		oos.writeObject(students);
+
+		oos.close();
+		fos.close();
+	}
+
+	public static Set<Student> studentLoader() throws IOException, ClassNotFoundException
+	{
+		FileInputStream fis = new FileInputStream("Files/students.txt");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+
+		Set<Student> students = (Set<Student>)ois.readObject();
+
+		ois.close();
+		fis.close();
+
+		return students;
 	}
 }
